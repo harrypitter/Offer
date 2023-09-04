ea34446ec2db6f69b7e3969c7d7803e0e2395ad5
harrypitter <harrypitter@163.com>
2023-09-04 14:14:19 +0800

    codecheck
    codecheck

--- src/CountDownLatchDemo/CountDownLatchDemo.java
+++ src/CountDownLatchDemo/CountDownLatchDemo.java
@@ -3,18 +3,17 @@ package CountDownLatchDemo;
 import java.util.concurrent.CountDownLatch;
 
 public class CountDownLatchDemo {
-    private final static int size = 3;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int size = 3;
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        for(int i=1;i<=3;i++){
-            new Thread(()->{
-                try{
+        for (int i = 1; i <= size; i++) {
+            new Thread(() -> {
+                try {
                     countDownLatch.await();
                     System.out.println(System.currentTimeMillis());
 
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }).start();
